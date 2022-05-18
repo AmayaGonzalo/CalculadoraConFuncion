707076cc6eca2084075f809e3e9fcd638aa49dfc
Amaya Gonzalo Javier <103613393+AmayaGonzalo@users.noreply.github.com>
2022-05-18 19:54:36 -0300

    CalculadoraconFuncion

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ let numero2: number;
 let opcionMenu: number;
 let resultado: number;
 
+//funcion que pide los dos números y la opcion de la operacion a realizar
 function pedirDatos() {
-  let i: number; //pide los dos números y la opcion de la operacion a realizar
+  let i: number;
   for (i = 0; i <= 3; i++) {
     if (i === 1) {
       numero1 = Number(prompt("Ingrese el " + i + "er número: "));
@@ -20,6 +21,7 @@ function pedirDatos() {
     }
   }
 }
+//funcion para dibujar 40 guiones seguidos
 function dibujarGuiones() {
   let i: number;
   let linea: string = "";
@@ -28,7 +30,7 @@ function dibujarGuiones() {
   }
   console.log(linea);
 }
-
+//funcion para realizar los calculos
 function calculaResultado() {
   if (opcionMenu === 1) {
     resultado = numero1 + numero2;
@@ -36,10 +38,11 @@ function calculaResultado() {
     resultado = numero1 - numero2;
   } else {
   }
-
   dibujarGuiones();
   console.log("El resultado de la operación es: " + resultado);
   dibujarGuiones();
 }
+
+//aca se ejecuta el programa:
 pedirDatos();
 calculaResultado();
